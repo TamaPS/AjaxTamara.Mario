df520b9ccb7ea3ab7ee3d2e0af28ef2371ef8d2e
TamaPS <55314943+TamaPS@users.noreply.github.com>
2020-02-17 19:25:22 +0100

    Casi página
    añadido: getResponseHeader('Pagination-Count'

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ function obtenerRespuesta(url) {
         xhr.setRequestHeader("x-api-key", "28b67f5f-39c3-449c-8a90-0c9dd8c36bf4");
         xhr.onload = function () {
             if (xhr.status == 200) {
-                var respuesta = xhr.response;
+                var respuesta = [xhr.getResponseHeader('Pagination-Count'), xhr.response];
                 resolve(respuesta);
             } else {
                 reject(Error(xhr.statusText));
@@ -24,7 +24,7 @@ obtenerRespuesta("https://api.thecatapi.com/v1/categories").then(
     manejarError
 );
 
-obtenerRespuesta("https://api.thecatapi.com/v1/images/search?page=1&limit=6&order=asc&category_ids=").then(
+obtenerRespuesta("https://api.thecatapi.com/v1/images/search?page=0&limit=6&order=asc&size=small&category_ids=").then(
     manejarRespuestaImages,
     manejarError
 );
@@ -32,25 +32,28 @@ obtenerRespuesta("https://api.thecatapi.com/v1/images/search?page=1&limit=6&orde
 var select = document.getElementById("cuadroLista");
 
 select.addEventListener("change", function() {
-    obtenerRespuesta("https://api.thecatapi.com/v1/images/search?page=1&limit=6&order=asc&category_ids="+select.value).then(
+    obtenerRespuesta("https://api.thecatapi.com/v1/images/search?page=0&limit=6&order=asc&size=small&category_ids="+select.value).then(
         manejarRespuestaImages,
         manejarError
     );
 });
 
 function manejarRespuestaCategories(respuesta) {
+    resCategorias = respuesta[1];
     var categorias = '<option value="">none</option>';
-    for (var i = 0; i < respuesta.length; i++) {
-        categorias += '<option value="' + respuesta[i].id + '">' + respuesta[i].name + '</option>';
+    for (var i = 0; i < resCategorias.length; i++) {
+        categorias += '<option value="' + resCategorias[i].id + '">' + resCategorias[i].name + '</option>';
     }
     document.getElementById("cuadroLista").innerHTML = categorias;
 }
 
 function manejarRespuestaImages(respuesta) {
+    numTotalImages = respuesta[0];
+    
+    resImages = respuesta[1];
     var fotos = '';
-    for (var i = 0; i < respuesta.length; i++) {
-        //fotos += '<div class="card square"><div class="card-body"><img class="card-img-top" width="200" src="' + respuesta[i].url + '" alt="' + respuesta[i].id + '"></div></div>';
-        fotos += '<div class="col-12 col-md-6 col-xl-4"><div class="card square  text-center"><img class="card-img-top" width="200" src="' + respuesta[i].url + '" alt="' + respuesta[i].id + '"></div></div>';
+    for (var i = 0; i < resImages.length; i++) {
+        fotos += '<div class="col-12 col-md-6 col-xl-4"><div class="card square  text-center"><img class="card-img-top" width="200" src="' + resImages[i].url + '" alt="' + resImages[i].id + '"></div></div>';
     }
     document.getElementsByClassName("row")[0].innerHTML = fotos;
 }
