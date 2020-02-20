c3248c46ccf120bfaec85e03bf126e77334616c5
TamaPS <55314943+TamaPS@users.noreply.github.com>
2020-02-20 12:03:47 +0100

    Finalizado

--- app.js
+++ app.js
@@ -3,18 +3,39 @@ var paginacion = document.getElementById("paginacion");
 var paginaActual = 0;
 var numPaginas;
 
+/*
+Cuando se abre la página se llama a la función obtener fotos, 
+sin categoría y en página 0 por defecto 
+*/
+obtenerFotos();
+
+/*
+Cuando se selecciona en el "cuadrolista" una categoría, 
+se vuelve a la página inicial
+y se llama a la función 
+"ObtenerFotos" 
+*/
 select.addEventListener("change", function () {
     paginaActual = 0;
     obtenerFotos();
 });
 
+/*
+Llama a la función obtener respuesta y le pasa los dos parámetros 
+(caso resolve y caso reject)
+y la API de las categorías
+*/
 obtenerRespuesta("https://api.thecatapi.com/v1/categories").then(
     manejarRespuestaCategories,
     manejarError
 );
 
-obtenerFotos();
-
+/*
+Función que recibe la url de la API de categorías.
+Devuelve una promesa, que redirige a 
+"manejarRespuestaCategories" o "manejarError"
+setRequestHeader es la clave que solicita la API
+*/
 function obtenerRespuesta(url) {
     return new Promise(function (resolve, reject) {
         var xhr = new XMLHttpRequest();
@@ -36,6 +57,9 @@ function obtenerRespuesta(url) {
     })
 }
 
+/*
+Función para cargar las fotos de una determinada categoría
+*/
 function obtenerFotos() {
     obtenerRespuesta("https://api.thecatapi.com/v1/images/search?page=" + paginaActual + "&limit=6&order=asc&size=small&category_ids=" + select.value).then(
         manejarRespuestaImages,
@@ -43,6 +67,10 @@ function obtenerFotos() {
     );
 }
 
+/*
+Función para recoger la categoría en el select
+por defecto es none
+*/
 function manejarRespuestaCategories(respuesta) {
     resCategorias = respuesta[1];
     var categorias = '<option value="">none</option>';
@@ -52,11 +80,22 @@ function manejarRespuestaCategories(respuesta) {
     document.getElementById("cuadroLista").innerHTML = categorias;
 }
 
+/*
+Función para cambiar de página
+y obtener fotos de dicha página
+Recibe la página que se quiere mostrar
+*/
 function cambiarPagina(numPagina) {
     paginaActual = numPagina;
     obtenerFotos();
 }
 
+/*
+Función para recoger las imágenes
+1º Recoge la imágenes que hay en cada categoría
+2º Define el número de páginas por categoría = 6 imágenes por x páginas
+3º Carga las imágenes en un contenedor row del html
+*/
 function manejarRespuestaImages(respuesta) {
     numTotalImages = respuesta[0];
     numPaginas = numTotalImages / 6
@@ -66,7 +105,9 @@ function manejarRespuestaImages(respuesta) {
         fotos += '<div class="col-12 col-md-6 col-xl-4"><div class="card square  text-center"><img class="card-img-top" width="200" src="' + resImages[i].url + '" alt="' + resImages[i].id + '"></div></div>';
     }
     document.getElementsByClassName("row")[0].innerHTML = fotos;
-
+    /*
+    SECCIÓN PARA CREAR LA PAGINACIÓN
+    */
     var a = document.createElement('a');
     a.href = '#';
     a.className = 'page-link';
@@ -77,12 +118,24 @@ function manejarRespuestaImages(respuesta) {
 
     paginacion.innerHTML = '';
 
+    /*
+    Las páginas avanzan y retroceden de 3 en 3
+    */
     paginaInicial = paginaActual - 3;
+
+    /*
+    Defina la página inicial como página 0 si
+    al restarle 3 es menor a 0
+    */
     if (paginaInicial < 0)
         paginaInicial = 0;
 
+    /*
+    Se añade el evento onclick a la página de retroceso
+    y llama a la función cambiar página pasándole la página actual -1
+    */   
     var a = document.createElement('a');
-    a.setAttribute('onclick', 'cambiarPagina(' + (paginaActual-1) + ')');
+    a.setAttribute('onclick', 'cambiarPagina(' + (paginaActual - 1) + ')');
     a.className = 'page-link';
     a.text = '<<';
     var li = document.createElement('li');
@@ -94,6 +147,9 @@ function manejarRespuestaImages(respuesta) {
     li.appendChild(a);
     paginacion.appendChild(li);
 
+    /*
+    Creación de los botones del 1 al 6
+    */   
     for (var i = paginaInicial; i < (paginaInicial + 6); i++) {
         if (i < numPaginas) {
             var a = document.createElement('a');
@@ -109,15 +165,18 @@ function manejarRespuestaImages(respuesta) {
             li.appendChild(a);
             paginacion.appendChild(li);
         }
-
     }
 
+    /*
+    Se añade el evento onclick a la página de avance
+    y llama a la función cambiar página pasándole la página actual +1
+    */   
     var a = document.createElement('a');
-    a.setAttribute('onclick', 'cambiarPagina(' + (paginaActual+1) + ')');
+    a.setAttribute('onclick', 'cambiarPagina(' + (paginaActual + 1) + ')');
     a.className = 'page-link';
     a.text = '>>';
     var li = document.createElement('li');
-    if (paginaActual < numPaginas-1)
+    if (paginaActual < numPaginas - 1)
         li.className = 'page-item';
     else {
         li.className = 'page-item disabled';
@@ -126,6 +185,11 @@ function manejarRespuestaImages(respuesta) {
     paginacion.appendChild(li);
 }
 
+
+/*
+Función a la que envía la llamada a la promesa 
+en caso de "manejarError"
+*/
 function manejarError() {
     console.log("**********Error*************");
 }
